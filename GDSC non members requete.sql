a2c2af6b9f542f79c7d95636cd84c2d24f08137f
mejrissi seif eddine <128096420+MejrissiSeifEddine@users.noreply.github.com>
2023-03-26 02:40:19 +0100

    Update GDSC non members requete.sql

--- GDSC non members requete.sql
+++ GDSC non members requete.sql
@@ -4,8 +4,8 @@ USE `GDSC members`;
 
 CREATE TABLE `MEMBERS` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `address` varchar(255) DEFAULT NULL,
+  `name` varchar(25) NOT NULL,
+  `email` varchar(50) NOT NULL,
+ 
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
